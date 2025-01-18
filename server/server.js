8614e33e6598cde61527b12dba5a805869a73eb0
EC2 Default User <ec2-user@ip-172-31-3-109.ap-south-1.compute.internal>
2025-01-18 17:47:39 +0000

    Update server.js to listen on 0.0.0.0

--- server/server.js
+++ server/server.js
@@ -183,7 +183,6 @@ io.on("connection", (socket) => {
     }
   });
 });
-
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {  // Bind to all network interfaces
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
