35025a75fb05b8d01d97eb785c5fad01f09a2987
AMAN RAJ <raj117557@gmail.com>
2025-01-19 17:51:23 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -11,8 +11,10 @@ const dbConnections = {
 async function connectToDb(req) {
   try {
     // Extract subdomain from request headers (e.g., "indiga" from "indiga.atithikripa.com")
-    const host = req.headers.host; // Get the full host (subdomain.domain.com)
-    const subdomain = host.split(".")[0]; // Extract the subdomain part
+    const host = req.headers.host; // Example: "indiga.atithikripa.com"
+    const subdomain = host.includes("www.")
+      ? host.split(".")[1]
+      : host.split(".")[0];
 
     // If the subdomain is 'www', you can handle it as an error or route it to a default subdomain
     if (subdomain === "www") {
